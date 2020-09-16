cc3a81d4d25c101123f4bef2a52ad071c6b9a240
Tan Theng Lee <thengleetan@gmail.com>
2020-09-16 15:27:27 +0800

    Render enquiry form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
 
 const ContactForm = () => {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  })
+
+  const sendEmail = event => {
+    event.preventDefault()
+
+    console.log('We will fill this up shortly.')
+    // code to trigger sending email
+  }
+
+  const onInputChange = event => {
+    const { name, value } = event.target
+
+    setState({
+      ...state,
+      [name]: value
+    })
+  }
+
   return (
     <div>
-      <h2>Contact Form</h2>
+      <form onSubmit={sendEmail}>
+        <Form.Group controlId="name">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            value={state.name}
+            placeholder="Enter your full name"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="text"
+            name="email"
+            value={state.email}
+            placeholder="Enter your email"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="subject">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control
+            type="text"
+            name="subject"
+            value={state.subject}
+            placeholder="Enter subject"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="message">
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            type="text"
+            name="message"
+            value={state.message}
+            placeholder="Enter your message"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </form>
     </div>
   )
 }
